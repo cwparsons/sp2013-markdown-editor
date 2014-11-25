9e6882e245cbb88cc658714cdb570d6638743a02
Christopher Parsons <cparsons@habaneroconsulting.com>
2014-11-24 20:31:46 -0800

    Added basic Gruntfile, package.json

---
+++ Gruntfile.js
@@ -0,0 +1,126 @@
+/*!
+ * Gruntfile for SP2013 Markdown Editor
+ * Christopher Parsons <cparsons@habaneroconsulting.com>
+ * Habanero Consulting Group - Licensed under MIT
+ */
+
+module.exports = function(grunt) {
+    'use strict';
+
+    /**
+     * Load required Grunt tasks.
+     */
+    require('load-grunt-tasks')(grunt);
+
+    /**
+     * Task configuration
+     */
+    var taskConfig = {
+        dirs: {
+            build: 'build',
+            source: 'source',
+            tmp: '.tmp',
+            vendor: '<%= dirs.source %>/vendor'
+        },
+
+        files: {
+            main: 'sp2013-markdown-editor.js'
+        },
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            build: '<%= dirs.build %>/*',
+            temp: '<%= dirs.tmp %>'
+        },
+
+        jshint: {
+            source: [
+                '<%= dirs.source %>/*.js'
+            ],
+            gruntfile: [
+                'Gruntfile.js'
+            ],
+            options: {
+                ignores: ['/**/*.min.js'],
+                curly: true,
+                immed: true,
+                newcap: false,
+                noarg: true,
+                debug: true,
+                sub: true,
+                boss: true,
+                eqnull: true,
+                multistr: true,
+                scripturl: true,
+                smarttabs: true,
+                '-W099': true,
+                loopfunc: true
+            }
+        },
+
+        uglify: {
+            plugin: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= dirs.source %>',
+                        src: [
+                            '**/*.js',
+                            '!vendor/**/*.js'
+                        ],
+                        dest: '<%= dirs.tmp %>/',
+                        ext: '.min.js'
+                    }
+                ],
+                options: {
+                    preserveComments: 'some'
+                }
+            }
+        },
+
+        concat: {
+            source: {
+                files: {
+                    '<%= dirs.build %>/sp2013-markdown-editor.min.js': [
+                        '<%= dirs.vendor %>/marked/lib/marked.js',
+                        '<%= dirs.tmp %>/editor.min.js'
+                    ]
+                },
+                options: {
+                    banner: '/*!\r\n * sp2013-markdown-editor.min.js\r\n * Habanero Consulting Group\r\n * Licensed under MIT \r\n */\r\n\r\n',
+                    separator: '\r\n\r\n'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['<%= dirs.source %>/**/*.js'],
+                tasks: ['build']
+            }
+        }
+    };
+
+    grunt.initConfig(taskConfig);
+
+
+    /**
+     * Register tasks
+     */
+    grunt.registerTask('bower', [
+        'shell'
+    ]);
+
+    grunt.registerTask('build', [
+        'clean', 'jshint', 'uglify', 'concat', 'clean:temp'
+    ]);
+
+    grunt.registerTask('serve', [
+        'build', 'watch'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-sp2013-markdown-editor
+Markdown Editor for SharePoint 2013
 ======================
 
 A Markdown editor to replace the rich text editor for SharePoint 2013.
+
+## Getting Started
+
+1. [Download the latest release of the script](https://github.com/habaneroconsulting/sp2013-markdown-editor/releases/latest).
+
+2. Upload the JavaScript file to your `Style Library`.
+
+3. Edit your masterpage and add a reference to the new file. For example:
+
+        <script src="/Style Library/Scripts/sp2013-markdown-editor.min.js"></script>
+
+4. Save and publish your masterpage.
+
+5. Go back to your portal and try it out.
+
+## Support
+
+If you have a bug, or a feature request, please post in the [issue tracker](https://github.com/habaneroconsulting/sp2013-markdown-editor/issues).
+
+## Build Instructions
+
+1. Install [NodeJS](http://nodejs.org/)
+
+2. Install Grunt CLI, bower, bower-installer.
+
+        npm -g install grunt-cli bower
+
+3. Run `npm install` from command line at root project folder
+
+4. Use `grunt build` to build, minify and concatenate the vendor and source files
+
+## License
+
+Copyright (c) 2014 Habanero Consulting Group
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
