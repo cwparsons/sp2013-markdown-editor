f76660b3be0b02228a081b9a16010278cc379782
Christopher Parsons <cparsons@habaneroconsulting.com>
2014-11-25 21:27:30 -0800

    Added saving of original Markdown code

--- source/editor.js
+++ source/editor.js
@@ -2,61 +2,78 @@
  *  editor.js
  */
 
+var ME = ME || {};
+
 (function() {
     'use strict';
 
+    var module = ME;
     var elements = {};
 
-
     function appendStyles() {
-        elements.head = document.getElementsByTagName('head')[0];
-
         var style = document.createElement('style');
-            style.innerHTML = '' +
-                '.me-markdown {' +
-                    'border-style: solid;' +
-                    'border-width: 1px;' +
-                    'display: none;' +
-                    'margin: 0 0 4px;' +
-                    'min-height: 400px;' +
-                    'padding: 5px' +
-                '}' +
 
-                '.me-markdown--activate .me-markdown {' +
-                    'display: block;' +
-                '}' +
+        style.innerHTML = '' +
+            '.me-markdown {' +
+                'border-style: solid;' +
+                'border-width: 1px;' +
+                'display: none;' +
+                'margin: 0 0 4px;' +
+                'min-height: 400px;' +
+                'padding: 5px' +
+            '}' +
 
-                '.me-markdown--activate .ms-formfieldvaluecontainer {' +
-                    'display: none;' +
-                '}' +
+            '.me-markdown--activate .me-markdown {' +
+                'display: block;' +
+            '}' +
 
-                '.me-markdown-textarea {' +
-                    '-moz-box-sizing: border-box;' +
-                    'box-sizing: border-box;' +
-                    'font-family: Consolas, monospace;' +
-                    'min-height: 400px;' +
-                    'width: 100%;' +
-                '}' +
+            '.me-markdown--activate .ms-formfieldvaluecontainer {' +
+                'display: none;' +
+            '}' +
+            
+            '.me-markdown--activate .ms-formfieldlabel:after {' +
+                'content: " (Markdown)";' +
+            '}' +
 
-                '.me-tab-container {' +
-                    'list-style: none;' +
-                    'margin: 0;' +
-                    'padding: 0;' +
-                '}' +
+            '.me-markdown-textarea {' +
+                '-moz-box-sizing: border-box;' +
+                'box-sizing: border-box;' +
+                'border: 0;' +
+                'font-family: Consolas, monospace;' +
+                'min-height: 410px;' +
+                'width: 100%;' +
+            '}' +
 
-                '.me-tab {' +
-                    'border: 1px solid #2A8DD4;' +
-                    'display: inline-block;' +
-                '}' +
+            '.me-tab-container {' +
+                'list-style: none;' +
+                'margin: 0;' +
+                'padding: 0;' +
+            '}' +
 
-                '.me-tab-link {' +
-                    'display: inline-block;' +
-                    'padding: 4px 8px;' +
-                '}';
+            '.me-tab {' +
+                'border: 1px solid #2A8DD4;' +
+                'display: inline-block;' +
+            '}' +
 
+            '.me-tab-link {' +
+                'cursor: pointer;' +
+                'display: inline-block;' +
+                'padding: 4px 8px;' +
+            '}' +
+
+            '.me-tab-link:hover {' +
+                'background-color: #0072C6;' +
+                'color: #fff;' +
+                'text-decoration: none;' +
+            '}';
+
+        elements.head = document.querySelector('head');
         elements.head.appendChild(style);
     }
-
+    
+    function cleanHtml(html) {
+        return html.replace('<br>', '').replace('<br />', '').replace('<br/>', '');
+    }
 
     function createTabs() {
         var html = '<div class="me-markdown ms-rte-border-field ms-rte-border"><textarea class="me-markdown-textarea"></textarea></div>' +
@@ -72,31 +89,33 @@
         elements.fields.appendChild(elements.editor);
     }
 
-
     function eventBindings() {
         elements.markdown = document.querySelector('a[data-connected="markdown"]');
 
         elements.markdown.addEventListener('click', function () {
             var className = ' me-markdown--activate ';
+            var textarea = document.querySelector('.me-markdown-textarea');
 
             if (elements.fields.className.indexOf(className) > -1) {
-                elements.fields.className = elements.fields.className.replace(className, ' ');
+                elements.fields.className = elements.fields.className.replace(className, ' ');               
 
-                var textarea = document.querySelector('.me-markdown-textarea');
+                var html = marked(textarea.value);
+                html += '<pre style="display: none;" class="noindex me-markdown-text">' + textarea.value + '</pre>';
 
-                elements.fields.querySelector('.ms-rtestate-write').innerHTML = marked(textarea.value);
+                elements.fields.querySelector('.ms-rtestate-write').innerHTML = html;
             } else {
                 elements.fields.className += className;
+
+                textarea.value = cleanHtml(elements.fields.querySelector('.me-markdown-text').innerHTML);
             }
         });
     }
 
-
-    function initalise() {
+    module.initalise = function () {
         appendStyles();
         createTabs();
         eventBindings();
-    }
+    };
 
-    initalise();
+    _spBodyOnLoadFunctionNames.push("ME.initalise");
 })();
