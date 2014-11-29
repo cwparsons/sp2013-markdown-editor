5f522a427e0e37a1b0035c3e0d16a95f152c3300
Christopher Parsons <cparsons@habaneroconsulting.com>
2014-11-29 02:37:41 -0800

    Added HTML to Markdown conversion

--- source/editor.js
+++ source/editor.js
@@ -1,4 +1,4 @@
-;(function(module, undefined) {
+(function(module) {
     'use strict';
 
     var classes = {
@@ -8,6 +8,30 @@
         },
         elements = {};
 
+    // Add the textarea for Markdown, and the toggling button
+    function appendHtml() {
+        var html =
+            '<div class="' + classes.prefix + '-textarea-container ms-rte-border-field ms-rte-border">' +
+                '<textarea class="' + classes.prefix + '-textarea"></textarea>' +
+            '</div>' +
+            '<ul class="' + classes.prefix + '-tab-list">' +
+                '<li class="' + classes.prefix + '-tab">' +
+                    '<a data-connected="markdown" class="' + classes.prefix + '-tab-link">Toggle Markdown</a>' +
+                '</li>' +
+            '</ul>';
+
+        elements.editor = document.createElement('div');
+        elements.editor.className = classes.prefix + '-editor-container';
+        elements.editor.innerHTML = html;
+
+        // TODO: Refactor to account for multiple rich text fields
+        elements.field = document.querySelector('[id*="ControlWrapper_RichHtmlField"]');
+        elements.field.className += ' ' + classes.prefix;
+        elements.field.appendChild(elements.editor);
+    }
+
+    // Add our button and editor styles via JavaScript to reduce file
+    // dependencies and ease overall installation.
     function appendStyles() {
         var style = document.createElement('style');
 
@@ -16,8 +40,13 @@
                 'position: relative;' +
             '}' +
 
+            // Hide the rich text area on activation
             '.' + classes.active + ' .ms-formfieldvaluecontainer {' +
                 'display: none;' +
+            '}' +
+
+            '.' + classes.prefix + ' .ms-formfieldlabel:after {' +
+                'content: " (Rich Text)";' +
             '}' +
 
             '.' + classes.active + ' .ms-formfieldlabel:after {' +
@@ -46,7 +75,7 @@
                 'width: 100%;' +
             '}' +
 
-            '.' + classes.prefix + '-tab-container {' +
+            '.' + classes.prefix + '-tab-list {' +
                 'list-style: none;' +
                 'margin: 0;' +
                 'padding: 0;' +
@@ -88,65 +117,70 @@
         elements.head.appendChild(style);
     }
 
-    function cleanHtml(html) {
-        return html.replace(/\<br( \/|\/)?\>/g, '');
-    }
-
-    function createTabs() {
-        var html =
-            '<div class="' + classes.prefix + '-textarea-container ms-rte-border-field ms-rte-border">' +
-                '<textarea class="' + classes.prefix + '-textarea"></textarea>' +
-            '</div>' +
-            '<ul class="' + classes.prefix + '-tab-container">' +
-                '<li class="' + classes.prefix + '-tab">' +
-                    '<a data-connected="markdown" class="' + classes.prefix + '-tab-link">Toggle Rich Text</a>' +
-                '</li>' +
-            '</ul>';
-
-        elements.editor = document.createElement('div');
-        elements.editor.className = classes.prefix + '-editor-container';
-        elements.editor.innerHTML = html;
-
-        elements.fields = document.querySelector('[id*="ControlWrapper_RichHtmlField"]');
-        elements.fields.className += ' ' + classes.prefix;
-        elements.fields.appendChild(elements.editor);
-    }
-
+    // Attach the click binding for the toggle button
     function eventBindings() {
         elements.markdown = document.querySelector('a[data-connected="markdown"]');
         elements.textarea = document.querySelector('.' + classes.prefix + '-textarea');
 
         elements.markdown.addEventListener('click', function () {
-            if (elements.fields.className.indexOf(classes.active) > -1) {
-                elements.fields.className = elements.fields.className.replace(' ' + classes.active, '');
-                elements.markdown.className = elements.markdown.className.replace(' ' + classes.tabActive, '');
-
-                elements.markdown.innerText = 'Toggle Rich Text';
-
-                var html = marked(elements.textarea.value);
-                html += '<pre style="display: none;" class="noindex ' + classes.prefix + '-text">' + elements.textarea.value + '</pre>';
-
-                elements.fields.querySelector('.ms-rtestate-write').innerHTML = html;
+            // Use the active CSS class for determining if we're on or not
+            if (elements.field.className.indexOf(classes.active) > -1) {
+                toggleRichText();
             } else {
-                elements.fields.className += ' ' + classes.active;
-                elements.markdown.className += ' ' + classes.tabActive;
-
-                elements.markdown.innerText = 'Toggle Markdown';
-
-                elements.textarea.value = cleanHtml(elements.fields.querySelector('.' + classes.prefix + '-text').innerHTML);
+                toggleMarkdown();
             }
         });
+    }
+
+    // Convert the Markdown into HTML, and show it in the rich text editor field
+    function toggleRichText() {
+        elements.field.className = elements.field.className.replace(' ' + classes.active, '');
+        elements.markdown.className = elements.markdown.className.replace(' ' + classes.tabActive, '');
+
+        elements.markdown.innerText = 'Toggle Markdown';
+
+        try {
+            var html = marked(elements.textarea.value);
+
+            if (html.length) {
+                elements.field.querySelector('.ms-rtestate-write').innerHTML = html;
+            }
+        } catch (exp) {
+            SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your Markdown to HTML');
+            console.log("Error while converting Markdown to HTML", exp);
+        }
+    }
+
+    // Convert the HTML to Markdown, and show it in the rich text editor field
+    function toggleMarkdown() {
+        elements.field.className += ' ' + classes.active;
+        elements.markdown.className += ' ' + classes.tabActive;
+
+        elements.markdown.innerText = 'Toggle Rich Text';
+
+        try {
+            var html = elements.field.querySelector('.ms-rtestate-write').innerHTML;
+
+            if (html.length) {
+                elements.textarea.value = toMarkdown(html);
+            }
+        } catch (exp) {
+            SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your HTML to Markdown');
+            console.log("Warning: Error while converting HTML to Markdown", exp);
+        }
     }
 
     module.initalise = function () {
         var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
 
+        // Only engage while in design (edit) mode
         if (inDesignMode) {
+            appendHtml();
             appendStyles();
-            createTabs();
             eventBindings();
         }
     };
 
+    // Wait for body load using an OOTB SharePoint object
     _spBodyOnLoadFunctionNames.push("ME.initalise");
 })(window.ME = window.ME || {});
