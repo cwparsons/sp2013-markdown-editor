7ea6bb8c50bd412385338b7f72886803e244da66
Christopher Parsons <cparsons@habaneroconsulting.com>
2015-01-11 13:54:50 -0800

    Added JSCS, updated files to match

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,172 +5,181 @@
  */
 
 module.exports = function(grunt) {
-    'use strict';
+	'use strict';
 
-    /**
-     * Load required Grunt tasks.
-     */
-    require('load-grunt-tasks')(grunt);
+	/**
+	 * Load required Grunt tasks.
+	 */
+	require('load-grunt-tasks')(grunt);
 
-    /**
-     * Task configuration
-     */
-    var taskConfig = {
-        dirs: {
-            build: 'build',
-            source: 'source',
-            tmp: '.grunt',
-            vendor: '<%= dirs.source %>/vendor'
-        },
+	/**
+	 * Task configuration
+	 */
+	var taskConfig = {
+		dirs: {
+			build: 'build',
+			source: 'source',
+			tmp: '.grunt',
+			vendor: '<%= dirs.source %>/vendor'
+		},
 
-        files: {
-            main: 'sp2013-markdown-editor.js'
-        },
+		files: {
+			main: 'sp2013-markdown-editor.js'
+		},
 
-        pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
-        clean: {
-            build: '<%= dirs.build %>/*',
-            temp: '<%= dirs.tmp %>'
-        },
+		clean: {
+			build: '<%= dirs.build %>/*',
+			temp: '<%= dirs.tmp %>'
+		},
 
-        cssmin: {
-            build: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= dirs.source %>',
-                        src: [
-                            '**/*.css'
-                        ],
-                        dest: '<%= dirs.tmp %>/',
-                        ext: '.css'
-                    }
-                ]
-            }
-        },
+		cssmin: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.source %>',
+						src: [
+							'**/*.css'
+						],
+						dest: '<%= dirs.tmp %>/',
+						ext: '.css'
+					}
+				]
+			}
+		},
 
-        css2js: {
-            build: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= dirs.tmp %>',
-                        src: [
-                            '**/*.css'
-                        ],
-                        dest: '<%= dirs.tmp %>/',
-                        ext: '-css.js'
-                    }
-                ]
-            }
-        },
+		css2js: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.tmp %>',
+						src: [
+							'**/*.css'
+						],
+						dest: '<%= dirs.tmp %>/',
+						ext: '-css.js'
+					}
+				]
+			}
+		},
 
-        jshint: {
-            source: [
-                '<%= dirs.source %>/*.js'
-            ],
-            gruntfile: [
-                'Gruntfile.js'
-            ],
-            options: {
-                ignores: ['/**/*.min.js'],
-                curly: true,
-                immed: true,
-                newcap: false,
-                noarg: true,
-                debug: true,
-                sub: true,
-                boss: true,
-                eqnull: true,
-                multistr: true,
-                scripturl: true,
-                smarttabs: true,
-                '-W099': true,
-                loopfunc: true
-            }
-        },
+		jscs: {
+			source: [
+				'<%= dirs.source %>/*.js'
+			],
+			options: {
+				config: './.jscsrc'
+			}
+		},
 
-        uglify: {
-            build: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: '<%= dirs.tmp %>',
-                        src: [
-                            '**/*.js'
-                        ],
-                        dest: '<%= dirs.tmp %>/',
-                        ext: '.js'
-                    },
-                    {
-                        expand: true,
-                        cwd: '<%= dirs.source %>',
-                        src: [
-                            '**/*.js',
-                            '!**/*.min.js'
-                        ],
-                        dest: '<%= dirs.tmp %>/',
-                        ext: '.js'
-                    }
-                ],
-                options: {
-                    preserveComments: require('uglify-save-license')
-                }
-            }
-        },
+		jshint: {
+			source: [
+				'<%= dirs.source %>/*.js'
+			],
+			gruntfile: [
+				'Gruntfile.js'
+			],
+			options: {
+				ignores: ['/**/*.min.js'],
+				curly: true,
+				immed: true,
+				newcap: false,
+				noarg: true,
+				debug: true,
+				sub: true,
+				boss: true,
+				eqnull: true,
+				multistr: true,
+				scripturl: true,
+				smarttabs: true,
+				'-W099': true,
+				loopfunc: true
+			}
+		},
 
-        concat: {
-            source: {
-                files: {
-                    '<%= dirs.build %>/sp2013-markdown-editor.min.js': [
-                        '<%= dirs.tmp %>/*.js',
-                        '<%= dirs.tmp %>/vendor/**/*.js'
-                    ]
-                },
-                options: {
-                    banner: '/*!\r\n * sp2013-markdown-editor.min.js\r\n * Habanero Consulting Group\r\n * Licensed under MIT\r\n */\r\n',
-                    separator: '\r\n\r\n'
-                }
-            }
-        },
+		uglify: {
+			build: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= dirs.tmp %>',
+						src: [
+							'**/*.js'
+						],
+						dest: '<%= dirs.tmp %>/',
+						ext: '.js'
+					},
+					{
+						expand: true,
+						cwd: '<%= dirs.source %>',
+						src: [
+							'**/*.js',
+							'!**/*.min.js'
+						],
+						dest: '<%= dirs.tmp %>/',
+						ext: '.js'
+					}
+				],
+				options: {
+					preserveComments: require('uglify-save-license')
+				}
+			}
+		},
 
-        shell: {
-            bower: {
-                options: {
-                    stdout: true
-                },
-                command: 'bower-installer'
-            }
-        },
+		concat: {
+			source: {
+				files: {
+					'<%= dirs.build %>/sp2013-markdown-editor.min.js': [
+						'<%= dirs.tmp %>/*.js',
+						'<%= dirs.tmp %>/vendor/**/*.js'
+					]
+				},
+				options: {
+					banner: '/*!\r\n * sp2013-markdown-editor.min.js\r\n * Habanero Consulting Group\r\n * Licensed under MIT\r\n */\r\n',
+					separator: '\r\n\r\n'
+				}
+			}
+		},
 
-        watch: {
-            scripts: {
-                files: ['<%= dirs.source %>/**/*.js'],
-                tasks: ['build']
-            }
-        }
-    };
+		shell: {
+			bower: {
+				options: {
+					stdout: true
+				},
+				command: 'bower-installer'
+			}
+		},
 
-    grunt.initConfig(taskConfig);
+		watch: {
+			scripts: {
+				files: ['<%= dirs.source %>/**/*.js'],
+				tasks: ['build']
+			}
+		}
+	};
+
+	grunt.initConfig(taskConfig);
 
 
-    /**
-     * Register tasks
-     */
-    grunt.registerTask('bower', [
-        'shell'
-    ]);
+	/**
+	 * Register tasks
+	 */
+	grunt.registerTask('bower', [
+		'shell'
+	]);
 
-    grunt.registerTask('build', [
-        'clean', 'jshint', 'cssmin', 'css2js', 'uglify', 'concat', 'clean:temp'
-    ]);
+	grunt.registerTask('build', [
+		'clean', 'jshint', 'jscs', 'cssmin', 'css2js', 'uglify', 'concat', 'clean:temp'
+	]);
 
-    grunt.registerTask('serve', [
-        'build', 'watch'
-    ]);
+	grunt.registerTask('serve', [
+		'build', 'watch'
+	]);
 
-    grunt.registerTask('default', [
-        'build'
-    ]);
+	grunt.registerTask('default', [
+		'build'
+	]);
 };
--- source/editor.js
+++ source/editor.js
@@ -1,175 +1,175 @@
-(function(module) {
-    'use strict';
+(function (module) {
+	'use strict';
 
-    var classes = {
-            prefix: 'markdown',
-            active: 'markdown--active',
-            tabActive: 'markdown-tab-link--active'
-        },
-        rtes = [];
+	var classes = {
+			prefix: 'markdown',
+			active: 'markdown--active',
+			tabActive: 'markdown-tab-link--active'
+		},
+		rtes = [];
 
-    // Add the textarea for Markdown, and the toggling button
-    function appendHtml () {
-        var html =
-            '<div class="' + classes.prefix + '-textarea-container ms-rte-border-field ms-rte-border">' +
-                '<textarea class="' + classes.prefix + '-textarea"></textarea>' +
-            '</div>' +
-            '<ul class="' + classes.prefix + '-tab-list">' +
-                '<li class="' + classes.prefix + '-tab">' +
-                    '<a data-connected="markdown" class="' + classes.prefix + '-tab-link">Toggle Markdown</a>' +
-                '</li>' +
-            '</ul>';
+	// Add the textarea for Markdown, and the toggling button
+	function appendHtml () {
+		var html =
+			'<div class="' + classes.prefix + '-textarea-container ms-rte-border-field ms-rte-border">' +
+				'<textarea class="' + classes.prefix + '-textarea"></textarea>' +
+			'</div>' +
+			'<ul class="' + classes.prefix + '-tab-list">' +
+				'<li class="' + classes.prefix + '-tab">' +
+					'<a data-connected="markdown" class="' + classes.prefix + '-tab-link">Toggle Markdown</a>' +
+				'</li>' +
+			'</ul>';
 
-        // Select all areas of the page that are rich text editors (page content
-        // or content editor web parts)
-        var fields = document.querySelectorAll('.ms-rtestate-write[contenteditable="true"]');
+		// Select all areas of the page that are rich text editors (page content
+		// or content editor web parts)
+		var fields = document.querySelectorAll('.ms-rtestate-write[contenteditable="true"]');
 
-        for (var i = 0; i < fields.length; i++) {
-            var ancestor = findFieldAncestor(fields[i]);
+		for (var i = 0; i < fields.length; i++) {
+			var ancestor = findFieldAncestor(fields[i]);
 
-            if (ancestor) {
-                ancestor.className += ' ' + classes.prefix;
+			if (ancestor) {
+				ancestor.className += ' ' + classes.prefix;
 
-                var editor = document.createElement('div');
-                editor.className = classes.prefix + '-editor-container';
-                editor.innerHTML = html;
+				var editor = document.createElement('div');
+				editor.className = classes.prefix + '-editor-container';
+				editor.innerHTML = html;
 
-                ancestor.appendChild(editor);
+				ancestor.appendChild(editor);
 
-                // Save references to the field, ancestor and editor for event
-                // bindings
-                var rte = {
-                    field: fields[i],
-                    ancestor: ancestor,
-                    textarea: editor.querySelector('.' + classes.prefix + '-textarea'),
-                    button: editor.querySelector('.' + classes.prefix + '-tab-link')
-                };
+				// Save references to the field, ancestor and editor for event
+				// bindings
+				var rte = {
+					field: fields[i],
+					ancestor: ancestor,
+					textarea: editor.querySelector('.' + classes.prefix + '-textarea'),
+					button: editor.querySelector('.' + classes.prefix + '-tab-link')
+				};
 
-                rtes.push(rte);
+				rtes.push(rte);
 
-                // Set the min-height to the textarea
-                rte.textarea.style.minHeight = rte.field.offsetHeight + 'px';
-            }
-        }
-    }
+				// Set the min-height to the textarea
+				rte.textarea.style.minHeight = rte.field.offsetHeight + 'px';
+			}
+		}
+	}
 
-    // Transform the HTML before going into Markdown mode
-    function cleanHtml (html) {
-        // Trim
-        html = html.trim();
+	// Transform the HTML before going into Markdown mode
+	function cleanHtml (html) {
+		// Trim
+		html = html.trim();
 
-        return html;
-    }
+		return html;
+	}
 
-    // Attach the click binding for the toggle button
-    function eventBindings () {
-        rtes.forEach(function (rte) {
-            // When the toggle button is clicked, switch between the two modes
-            rte.button.addEventListener('click', function () {
-                if (hasClass(rte.ancestor, classes.active)) {
-                    toggleRichText(rte);
-                } else {
-                    toggleMarkdown(rte);
-                }
-            });
+	// Attach the click binding for the toggle button
+	function eventBindings () {
+		rtes.forEach(function (rte) {
+			// When the toggle button is clicked, switch between the two modes
+			rte.button.addEventListener('click', function () {
+				if (hasClass(rte.ancestor, classes.active)) {
+					toggleRichText(rte);
+				} else {
+					toggleMarkdown(rte);
+				}
+			});
 
-            rte.textarea.addEventListener('keyup', function () {
-                saveMarkdown(rte.textarea, rte.field);
-            });
+			rte.textarea.addEventListener('keyup', function () {
+				saveMarkdown(rte.textarea, rte.field);
+			});
 
-            rte.textarea.addEventListener('change', function () {
-                saveMarkdown(rte.textarea, rte.field);
-            });
-        });
-    }
+			rte.textarea.addEventListener('change', function () {
+				saveMarkdown(rte.textarea, rte.field);
+			});
+		});
+	}
 
-    // Return the appropriate parent container for the rich text editor
-    function findFieldAncestor (element) {
-        // If it is a page content, find one parent
-        var ancestor = findAncestor(element, 'ms-rtestate-field');
+	// Return the appropriate parent container for the rich text editor
+	function findFieldAncestor (element) {
+		// If it is a page content, find one parent
+		var ancestor = findAncestor(element, 'ms-rtestate-field');
 
-        // If it is a content editor web part, the above ancestor class
-        // won't exist
-        if (!ancestor) {
-            return findAncestor(element, 'ms-webpart-chrome');
-        }
+		// If it is a content editor web part, the above ancestor class
+		// won't exist
+		if (!ancestor) {
+			return findAncestor(element, 'ms-webpart-chrome');
+		}
 
-        return ancestor;
-    }
+		return ancestor;
+	}
 
-    // Return the first parent node that has the given class name
-    function findAncestor (element, className) {
-        while ((element = element.parentElement) && !hasClass(element, className)) {
-            continue;
-        }
+	// Return the first parent node that has the given class name
+	function findAncestor (element, className) {
+		while ((element = element.parentElement) && !hasClass(element, className)) {
+			continue;
+		}
 
-        return element;
-    }
+		return element;
+	}
 
-    // Return true if an element has the given class name
-    function hasClass (element, className) {
-        if (element && element instanceof HTMLElement) {
-            return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
-        }
+	// Return true if an element has the given class name
+	function hasClass (element, className) {
+		if (element && element instanceof HTMLElement) {
+			return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    // Convert Markdown to HTML and copy HTML content into the RTE
-    function saveMarkdown (textarea, field) {
-        var html = marked(textarea.value);
+	// Convert Markdown to HTML and copy HTML content into the RTE
+	function saveMarkdown (textarea, field) {
+		var html = marked(textarea.value);
 
-        if (html.length) {
-            field.innerHTML = html;
-        }
-    }
+		if (html.length) {
+			field.innerHTML = html;
+		}
+	}
 
-    // Convert the Markdown into HTML, and show it in the rich text editor field
-    function toggleRichText (rte) {
-        rte.ancestor.className = rte.field.className.replace(' ' + classes.active, '');
-        rte.button.innerText = 'Toggle Markdown';
+	// Convert the Markdown into HTML, and show it in the rich text editor field
+	function toggleRichText (rte) {
+		rte.ancestor.className = rte.field.className.replace(' ' + classes.active, '');
+		rte.button.innerText = 'Toggle Markdown';
 
-        try {
-            saveMarkdown(rte.textarea, rte.field);
-        } catch (exp) {
-            SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your Markdown to HTML');
-            console.log('Error while converting Markdown to HTML', exp);
-        }
-    }
+		try {
+			saveMarkdown(rte.textarea, rte.field);
+		} catch (exp) {
+			SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your Markdown to HTML');
+			console.log('Error while converting Markdown to HTML', exp);
+		}
+	}
 
-    // Convert the HTML to Markdown, and show it in the rich text editor field
-    function toggleMarkdown (rte) {
-        rte.ancestor.className += ' ' + classes.active;
-        rte.button.innerText = 'Toggle Rich Text';
+	// Convert the HTML to Markdown, and show it in the rich text editor field
+	function toggleMarkdown (rte) {
+		rte.ancestor.className += ' ' + classes.active;
+		rte.button.innerText = 'Toggle Rich Text';
 
-        try {
-            var html = rte.field.innerHTML;
+		try {
+			var html = rte.field.innerHTML;
 
-            if (html.length) {
-                html = toMarkdown(html);
-                rte.textarea.value = cleanHtml(html);
-            }
-        } catch (exp) {
-            SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your HTML to Markdown');
-            console.log('Error while converting HTML to Markdown', exp);
-        }
-    }
+			if (html.length) {
+				html = toMarkdown(html);
+				rte.textarea.value = cleanHtml(html);
+			}
+		} catch (exp) {
+			SP.UI.Notify.addNotification('<strong>Warning:</strong> There was an error while converting your HTML to Markdown');
+			console.log('Error while converting HTML to Markdown', exp);
+		}
+	}
 
-    module.init = function () {
-        var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
+	module.init = function () {
+		var inDesignMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
 
-        // Only engage while in design (edit) mode
-        if (inDesignMode) {
-            appendHtml();
-            eventBindings();
-        }
-    };
+		// Only engage while in design (edit) mode
+		if (inDesignMode) {
+			appendHtml();
+			eventBindings();
+		}
+	};
 
-    if (_spBodyOnLoadCalled) {
-        module.init();
-    } else {
-        // Wait for body load using an OOTB SharePoint object
-        _spBodyOnLoadFunctionNames.push('MD.init');
-    }
+	if (_spBodyOnLoadCalled) {
+		module.init();
+	} else {
+		// Wait for body load using an OOTB SharePoint object
+		_spBodyOnLoadFunctionNames.push('MD.init');
+	}
 
 })(window.MD = window.MD || {});
